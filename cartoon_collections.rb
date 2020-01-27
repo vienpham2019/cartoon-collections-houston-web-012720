32f535ab2c8966757ff4b944106512d3b618110f
vien pham <vienpham2019@gmail.com>
2020-01-27 21:24:32 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,8 +3,9 @@ def roll_call_dwarves(array)# code an argument here
   array.each_with_index{|element,index| puts "#{index + 1} #{element}"}
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(planeteer_calls)# code an argument here
   # Your code here
+  newArr = planeteer_calls
 end
 
 def long_planeteer_calls# code an argument here
