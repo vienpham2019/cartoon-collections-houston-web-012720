180202bc347102729ec62b1c54fc95334900e41d
vien pham <vienpham2019@gmail.com>
2020-01-27 21:46:38 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,7 +15,9 @@ def long_planeteer_calls(short_words)# code an argument here
   word ? true : false
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(ingredients)# code an argument here
   # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
+  cheese_types = ["cheddar", "gouda", "camembert"];
+  cheese = ingredients.find{|food| cheese_types.include?(food)}
+  return cheese
 end
