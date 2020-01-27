c4fc633051dea7fb6c998d5b8a57e1b2ff8b643f
vien pham <vienpham2019@gmail.com>
2020-01-27 21:30:51 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,7 +5,7 @@ end
 
 def summon_captain_planet(planeteer_calls)# code an argument here
   # Your code here
-  newArr = planeteer_calls
+  planeteer_calls.collect{|element| element.capitalize}
 end
 
 def long_planeteer_calls# code an argument here
