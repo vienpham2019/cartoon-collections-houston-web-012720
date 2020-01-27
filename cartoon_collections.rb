f45d7627f06619a713dac7787c46feb0dd5f203e
vien pham <vienpham2019@gmail.com>
2020-01-27 21:19:25 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,7 +1,6 @@
 def roll_call_dwarves(array)# code an argument here
   # Your code here
   array.each_with_index{|element,index| puts `#{index} #{element}`}
-  
 end
 
 def summon_captain_planet# code an argument here
